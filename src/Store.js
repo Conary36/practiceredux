bd9a61eabf55fd18a6cb94110c94cd8b23161c9b
Conary36 <cbeckford144@gmail.com>
2020-09-07 21:18:11 -0400

    ...saving3..

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux"; //Needed for applyMiddleware
+import { createStore, applyMiddleware, compose } from "redux"; //Needed for applyMiddleware
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
@@ -7,7 +7,9 @@ const initialState = {}
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware))
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__() //Required for Redux DevTools
+))
 
 export default store;
 
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect} from 'react-redux'
 import {fetchPosts} from '../actions/postActions'
 
@@ -8,7 +9,7 @@ import {fetchPosts} from '../actions/postActions'
   }
     
     render() {
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
             <div key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
@@ -22,4 +23,13 @@ import {fetchPosts} from '../actions/postActions'
         )
     }
 }
-export default connect(null, {fetchPosts})(Posts)
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired
+}
+
+
+const mapStateToProps = state => ({
+    posts: state.posts.items
+});
+export default connect(mapStateToProps, {fetchPosts})(Posts)
